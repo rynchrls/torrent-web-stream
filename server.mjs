1025a8c534d7ac00040c9faf7149d0f6fd24bc69
Ryan Charles <rynchrls18@gmail.com>
2025-05-02 11:27:04 +0800

    testing

--- server.mjs
+++ server.mjs
@@ -1,11 +1,20 @@
 import express from "express";
 import cors from "cors";
-import parseTorrent from "parse-torrent";
+import path from "path";
 import { handler, destroyTorrent } from "./webTorrent.mjs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Serve the HTML file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.get("/torrent", handler);
 app.post("/destroy", destroyTorrent);
